6ce73ba9ebaa7168125556b708edbb80568e1cdc
DaGan <775935087@qq.com>
2020-03-03 07:01:58 +0800

    用chai写单元测试

--- src/app.js
+++ src/app.js
@@ -13,3 +13,35 @@ new Vue({
     loading1: false
   }
 });
+
+import chai from "chai";
+const expect = chai.expect;
+
+{
+  const Constructor = Vue.extend(Button);
+  const button = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  });
+  button.$mount("#test");
+  let useElement = button.$el.querySelector("use");
+  expect(useElement.getAttribute("xlink:href")).to.eq("#i-settings");
+  button.$el.remove();
+  button.$destory();
+}
+
+{
+  const Constructor = Vue.extend(Button);
+  const button = new Constructor({
+    propsData: {
+      icon: "settings",
+      loading: true
+    }
+  });
+  button.$mount();
+  let useElement = button.$el.querySelector("use");
+  expect(useElement.getAttribute("xlink:href")).to.eq("#i-loading");
+  button.$el.remove();
+  button.$destory();
+}
