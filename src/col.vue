16c7f55526ae9e03b6aa79fa8467c5218989e5bd
DaGan <775935087@qq.com>
2020-03-10 06:06:02 +0800

    row 和 col 雏形

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.col {
+  height: 100px;
+  background: blue;
+  border: 1px solid red;
+  box-sizing: border-box;
+  $class-prefix: col-;
+  @for $n from 1 through 20 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
