273441dd79b02b9c68e7e171e0b349bdaefb2d43
qin.zehua <775935087@qq.com>
2020-06-23 20:43:24 +0800

    添加toast plugin

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import Vue from "vue";
+import Plugin from "./src/plugin";
 import Button from "./src/button.vue";
 import ButtonGroup from "./src/button-group.vue";
 import Input from "./src/input.vue";
 import Row from "./src/row.vue";
 import Col from "./src/col.vue";
-
 import Content from "./src/layout/content";
 import Footer from "./src/layout/footer";
 import Header from "./src/layout/header";
 import Layout from "./src/layout/layout";
 import Slider from "./src/layout/slider";
+import Toast from "./src/toast/toast";
 
 Vue.component("g-button-group", ButtonGroup);
 Vue.component("g-button", Button);
@@ -21,6 +22,8 @@ Vue.component("g-footer", Footer);
 Vue.component("g-header", Header);
 Vue.component("g-layout", Layout);
 Vue.component("g-slider", Slider);
+Vue.component("g-toast", Toast);
+Vue.use(Plugin);
 
 new Vue({
   el: "#app",
@@ -31,6 +34,9 @@ new Vue({
   methods: {
     inputChange(val) {
       console.log(val);
+    },
+    showToast() {
+      this.$toast("这是toast");
     },
   },
 });
---
+++ src/toast/toast.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="toast">
+    <slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.toast {
+  border: 1px solid;
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  line-height: 1.8;
+  height: $toast-height;
+  display: flex;
+  align-items: center;
+  background-color: $toast-bg;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  color: white;
+  padding: 0 16px;
+}
+</style>
