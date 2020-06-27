fdc5ea3dfb499378a7b434db444c86c7f3d06274
qin.zehua <775935087@qq.com>
2020-06-27 17:24:13 +0800

    通过计算属性简单实现级联选择

--- src/cascader/cascader.vue
+++ src/cascader/cascader.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
+    <div class="trigger" @click="cascaderVisible = !cascaderVisible">
       <slot></slot>
     </div>
-    <div class="popover">
-      <Cascader-x :options="options"></Cascader-x>
+    <div class="popover" v-if="cascaderVisible">
+      <div class="level level1">
+        <div
+          v-for="(item1, idx) in options"
+          :key="idx"
+          @click="level1Selected = item1"
+        >
+          {{ item1.name }}
+        </div>
+      </div>
+      <div class="level level2">
+        <div
+          v-for="(item2, idx) in level2Items"
+          :key="idx"
+          @click="level2Selected = item2"
+        >
+          {{ item2.name }}
+        </div>
+      </div>
+      <div class="level level3">
+        <div v-for="(item3, idx) in level3Items" :key="idx">
+          {{ item3.name }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,11 +43,37 @@ export default {
       type: Array,
     },
   },
+  data() {
+    return {
+      cascaderVisible: false,
+      level1Selected: null,
+      level2Selected: null,
+    };
+  },
+  computed: {
+    level2Items() {
+      if (this.level1Selected) {
+        return this.level1Selected.children;
+      } else {
+        return [];
+      }
+    },
+    level3Items() {
+      if (this.level2Selected) {
+        return this.level2Selected.children;
+      } else {
+        return [];
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../var";
 .cascader {
+  .popover {
+    display: flex;
+  }
 }
 </style>
