f5b1096bead5ca437e98e1e7028187cf27427a39
qin.zehua <775935087@qq.com>
2020-06-27 14:37:20 +0800

    提取scss变量到var.scss

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cascader">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CasCader",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../var";
+.cascader {
+}
+</style>
