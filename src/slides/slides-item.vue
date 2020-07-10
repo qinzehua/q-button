2fe3856d35c8bf52caf2b99c9e66a36154e8479e
qin.zehua <775935087@qq.com>
2020-07-11 07:50:49 +0800

    创建组件

---
+++ src/slides/slides-item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="g-slides-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "g-slide-item"
+};
+</script>
+
+<style>
+.g-slides-item {
+}
+</style>
---
+++ src/slides/slides.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="g-slides">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "g-slide"
+};
+</script>
+
+<style>
+.g-slides {
+}
+</style>
